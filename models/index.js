3d9de1777c1ec3c64bac80d840fd827d4a870fcd
Jeremy Jacobson <jjacobson93@gmail.com>
2014-08-06 08:21:00 -0700

    DB

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ var fs        = require('fs'),
 	path      = require('path'),
 	Sequelize = require('sequelize'),
 	lodash    = require('lodash'),
-	sequelize = new Sequelize('javelin', 'jjacobson', null, {
+	sequelize = new Sequelize('javelin', 'postgres', 'postmaster', {
 		dialect: 'postgres',
 		define: {
 			underscored: true
